b9f4c7dd94556e7b5c78ba7f44c2bd97ae8b60ef
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-24 21:50:39 +0530

    Update App.js File

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import LoginPage from "./LoginPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/>
-      <Home/> */}
       <LoginPage/>
-      {/* <Footer/> */}
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
