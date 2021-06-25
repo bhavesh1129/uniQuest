8bc7c5f9126006e825cf56604a7e2119b206ecd7
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-25 19:09:52 +0530

    Add Users Files

---
+++ src/Users.js
@@ -0,0 +1,251 @@
+import React from "react";
+
+function Users() {
+  return (
+    <div>
+      <div id="container" class="w-4/5 mx-auto">
+        <div class="flex flex-col sm:flex-row">
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/cool--v2.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Rahul Yadav</h2>
+              <span class="text-blue-500 block">B.Tech</span>
+              <span class="text-blue-500 block mt-0 mb-2">2nd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/lol-surprise.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Alisha Garg</h2>
+              <span class="text-blue-500 block">B.Com</span>
+              <span class="text-blue-500 block mt-0 mb-2">1st Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/user-male-circle.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Aman Goel</h2>
+              <span class="text-blue-500 block ">B.Tech</span>
+              <span class="text-blue-500 block mt-0 mb-2">3rd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/bot.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Arshiya Bansal</h2>
+              <span class="text-blue-500 block">B.Arch</span>
+              <span class="text-blue-500 block mt-0 mb-2">4th Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="container" class="w-4/5 mx-auto">
+        <div class="flex flex-col sm:flex-row">
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/user-male-circle.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Prashant Gupta</h2>
+              <span class="text-blue-500 block">B.Tech</span>
+              <span class="text-blue-500 block mt-0 mb-2">1st Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/businesswoman.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Shreya Bansal</h2>
+              <span class="text-blue-500 block">BCA</span>
+              <span class="text-blue-500 block mt-0 mb-2">2nd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/asian-character.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Mayank Garg</h2>
+              <span class="text-blue-500 block ">B.Tech</span>
+              <span class="text-blue-500 block mt-0 mb-2">2nd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/cat-profile.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Radhika Goyal</h2>
+              <span class="text-blue-500 block ">B.Tech</span>
+              <span class="text-blue-500 block mt-0 mb-2">3rd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="container" class="w-4/5 mx-auto">
+        <div class="flex flex-col sm:flex-row">
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/user-male-circle.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Panav Garg</h2>
+              <span class="text-blue-500 block ">BCA</span>
+              <span class="text-blue-500 block mt-0 mb-2">3rd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/brave-web-browser.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">
+                Rohanpreet Singh
+              </h2>
+              <span class="text-blue-500 block">B.Arch</span>
+              <span class="text-blue-500 block mt-0 mb-2">3rd Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/user-female-circle.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Kirti Kushwaha</h2>
+              <span class="text-blue-500 block">BSC</span>
+              <span class="text-blue-500 block mt-0 mb-2">4th Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+
+          <div class="sm:w-1/4 p-2">
+            <div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
+              <div class="mb-3">
+                <img
+                  class="w-auto mx-auto rounded-full"
+                  src="https://img.icons8.com/ios-glyphs/150/000000/anonymous-mask.png"
+                  alt=""
+                />
+              </div>
+              <h2 class="text-xl font-medium text-gray-700">Rohan</h2>
+              <span class="text-blue-500 block">M.Tech</span>
+              <span class="text-blue-500 block mt-0 mb-2">1st Year</span>
+
+              <a href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full">
+                Chat
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Users;
