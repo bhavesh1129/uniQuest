ec67035fc6541861a8032862a4e9d7742b113e9f
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-25 19:08:47 +0530

    Add Recent Threads File

---
+++ src/RecentThreads.js
@@ -0,0 +1,423 @@
+import React from "react";
+
+function RecentThreads() {
+  return (
+    <div>
+      <div class="container items-center px-5 text-blueGray-500 lg:px-20">
+        <div class="p-2 mx-auto my-6 bg-white border rounded-lg shadow-xl lg:w-3/4">
+          <div class="flex-grow p-6 py-2 rounded-lg">
+            <div class="text-left text-xs font-semibold text-gray-500">June25,2021</div>
+            <p class="mb-3 text-base leading-relaxed text-blueGray-500">
+              If any Environment Variables values are changed between
+              Deployments, deduplication will always be bypassed. So you know.
+            </p>
+            <div class="flex flex-wrap justify-between">
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="read more"
+              >
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+                  Answer
+                </button>
+              </a>
+              <a
+                href="#"
+                class="mx-4 ml-auto text-red-600 hover:text-black"
+                title="like"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-heart"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="share"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-share"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <circle cx="6" cy="12" r="3"></circle>
+                  <circle cx="18" cy="6" r="3"></circle>
+                  <circle cx="18" cy="18" r="3"></circle>
+                  <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
+                  <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container items-center px-5 text-blueGray-500 lg:px-20">
+        <div class="p-2 mx-auto my-6 bg-white border rounded-lg shadow-xl lg:w-3/4">
+          <div class="flex-grow p-6 py-2 rounded-lg">
+          <div class="text-left text-xs font-semibold text-gray-500">June25,2021</div>
+            <p class="mb-3 text-base leading-relaxed text-blueGray-500">
+              If any Environment Variables values are changed between
+              Deployments, deduplication will always be bypassed. So you know.
+            </p>
+            <div class="flex flex-wrap justify-between">
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="read more"
+              >
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+                  Answer
+                </button>
+              </a>
+              <a
+                href="#"
+                class="mx-4 ml-auto text-red-600 hover:text-black"
+                title="like"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-heart"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="share"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-share"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <circle cx="6" cy="12" r="3"></circle>
+                  <circle cx="18" cy="6" r="3"></circle>
+                  <circle cx="18" cy="18" r="3"></circle>
+                  <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
+                  <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container items-center px-5 text-blueGray-500 lg:px-20">
+        <div class="p-2 mx-auto my-6 bg-white border rounded-lg shadow-xl lg:w-3/4">
+          <div class="flex-grow p-6 py-2 rounded-lg">
+          <div class="text-left text-xs font-semibold text-gray-500">June25,2021</div>
+            <p class="mb-3 text-base leading-relaxed text-blueGray-500">
+              If any Environment Variables values are changed between
+              Deployments, deduplication will always be bypassed. So you know.
+            </p>
+            <div class="flex flex-wrap justify-between">
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="read more"
+              >
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+                  Answer
+                </button>
+              </a>
+              <a
+                href="#"
+                class="mx-4 ml-auto text-red-600 hover:text-black"
+                title="like"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-heart"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="share"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-share"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <circle cx="6" cy="12" r="3"></circle>
+                  <circle cx="18" cy="6" r="3"></circle>
+                  <circle cx="18" cy="18" r="3"></circle>
+                  <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
+                  <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container items-center px-5 text-blueGray-500 lg:px-20">
+        <div class="p-2 mx-auto my-6 bg-white border rounded-lg shadow-xl lg:w-3/4">
+          <div class="flex-grow p-6 py-2 rounded-lg">
+          <div class="text-left text-xs font-semibold text-gray-500">June25,2021</div>
+            <p class="mb-3 text-base leading-relaxed text-blueGray-500">
+              If any Environment Variables values are changed between
+              Deployments, deduplication will always be bypassed. So you know.
+            </p>
+            <div class="flex flex-wrap justify-between">
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="read more"
+              >
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+                  Answer
+                </button>
+              </a>
+              <a
+                href="#"
+                class="mx-4 ml-auto text-red-600 hover:text-black"
+                title="like"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-heart"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="share"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-share"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <circle cx="6" cy="12" r="3"></circle>
+                  <circle cx="18" cy="6" r="3"></circle>
+                  <circle cx="18" cy="18" r="3"></circle>
+                  <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
+                  <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container items-center px-5 text-blueGray-500 lg:px-20">
+        <div class="p-2 mx-auto my-6 bg-white border rounded-lg shadow-xl lg:w-3/4">
+          <div class="flex-grow p-6 py-2 rounded-lg">
+          <div class="text-left text-xs font-semibold text-gray-500">June25,2021</div>
+            <p class="mb-3 text-base leading-relaxed text-blueGray-500">
+              If any Environment Variables values are changed between
+              Deployments, deduplication will always be bypassed. So you know.
+            </p>
+            <div class="flex flex-wrap justify-between">
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="read more"
+              >
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+                  Answer
+                </button>
+              </a>
+              <a
+                href="#"
+                class="mx-4 ml-auto text-red-600 hover:text-black"
+                title="like"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-heart"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="share"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-share"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <circle cx="6" cy="12" r="3"></circle>
+                  <circle cx="18" cy="6" r="3"></circle>
+                  <circle cx="18" cy="18" r="3"></circle>
+                  <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
+                  <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container items-center px-5 text-blueGray-500 lg:px-20">
+        <div class="p-2 mx-auto my-6 bg-white border rounded-lg shadow-xl lg:w-3/4">
+          <div class="flex-grow p-6 py-2 rounded-lg">
+          <div class="text-left text-xs font-semibold text-gray-500">June25,2021</div>
+            <p class="mb-3 text-base leading-relaxed text-blueGray-500">
+              If any Environment Variables values are changed between
+              Deployments, deduplication will always be bypassed. So you know.
+            </p>
+            <div class="flex flex-wrap justify-between">
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="read more"
+              >
+                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-full">
+                  Answer
+                </button>
+              </a>
+              <a
+                href="#"
+                class="mx-4 ml-auto text-red-600 hover:text-black"
+                title="like"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-heart"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+                </svg>
+              </a>
+              <a
+                href="#"
+                class="text-sm font-semibold text-blue-600 uppercase hover:text-black"
+                title="share"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-share"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <circle cx="6" cy="12" r="3"></circle>
+                  <circle cx="18" cy="6" r="3"></circle>
+                  <circle cx="18" cy="18" r="3"></circle>
+                  <line x1="8.7" y1="10.7" x2="15.3" y2="7.3"></line>
+                  <line x1="8.7" y1="13.3" x2="15.3" y2="16.7"></line>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RecentThreads;
